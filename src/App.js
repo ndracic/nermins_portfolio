e2f12d29de53dcab493a20c76ab40c3008d642e9
Nerms <Nerms@Nermins-MacBook-Pro.local>
2023-04-08 12:37:51 -0500

    added all components to create single page po rtfolio, however contact displays twice when pressed. need fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer'
 import background from './images/bg.jpeg'
+import Resume from './components/Resume';
+import Aboutme from './components/Aboutme';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Homepage from './components/Homepage';
 // import Aboutme from './components/Aboutme';
 // import Portfolio from './components/Portfolio';
 // import Resume from './components/Resume';
@@ -12,7 +17,11 @@ function App() {
   return (
     <div className="App" style={{backgroundImage: `url(${background})`}}>
       <Navbar />
-
+      <Homepage />
+      <Resume />
+      <Aboutme />
+      <Portfolio />
+      <Contact />
       <Footer />
     </div>
   );
